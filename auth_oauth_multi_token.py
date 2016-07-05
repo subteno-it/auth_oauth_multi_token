a225d62ae36f572b965512a3f008777a774c57ac
IguanaYachts <florent@iguanayachts.com>
2016-07-06 01:55:05 +0200

    [FIX] remove old token [NEW] add field in res.user to choose the number of simultanous connection

--- auth_oauth_multi_token.py
+++ auth_oauth_multi_token.py
@@ -15,37 +15,51 @@ class auth_oauth_multi_token(models.Model):
 
     oauth_access_token = fields.Char('OAuth Access Token', readonly=True, copy=False)
     user_id = fields.Many2one('res.users', 'User', required=True)
-    active = fields.Boolean('Active')
+    active_token = fields.Boolean('Active')
+
+    @api.model
+    def create(self, vals):
+        res = super(auth_oauth_multi_token, self).create(vals)
+        oauth_access_token_ids = self.search([('user_id' ,'=', vals['user_id']), ('active_token', '=', True)], ).ids
+        oauth_access_max_token = self.env['res.users'].search([('id', '=', vals['user_id'])], limit=1).oauth_access_max_token
+        if len(oauth_access_token_ids) >= oauth_access_max_token:
+            self.browse(oauth_access_token_ids[oauth_access_max_token]).write({
+                            'oauth_access_token':"****************************",
+                            'active_token': False})
+
+        return res
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    oauth_access_token_ids = fields.One2many('auth.oauth.multi.token', 'user_id', 'Work Centers', copy=True)
+    oauth_access_token_ids = fields.One2many('auth.oauth.multi.token', 'user_id', 'Tokens', copy=False)
     oauth_access_max_token = fields.Integer('Number of simultaneous connection', default=5, required=True)
 
     @api.model
     def _auth_oauth_signin(self, provider, validation, params):
         res = super(ResUsers, self)._auth_oauth_signin(provider, validation, params)
-        try:
-            oauth_uid = validation['user_id']
-            user_ids = self.search([("oauth_uid", "=", oauth_uid), ('oauth_provider_id', '=', provider)]).ids
-            if not user_ids:
-                raise openerp.exceptions.AccessDenied()
-            assert len(user_ids) == 1
 
-            self.oauth_access_token_ids.create({'user_id': user_ids[0],
-                                                'oauth_access_token': params['access_token'],
-                                                'active': true,
-                                                })
-            self.oauth_access_token_ids.browse(self.oauth_access_token_ids.ids[self.oauth_access_max_token]).write({
-                'oauth_access_token': "*********",
-                'active': False,
-            })
+        oauth_uid = validation['user_id']
+        user_ids = self.search([("oauth_uid", "=", oauth_uid), ('oauth_provider_id', '=', provider)]).ids
+        if not user_ids:
+            raise openerp.exceptions.AccessDenied()
+        assert len(user_ids) == 1
 
-        except:
-            pass
+        self.oauth_access_token_ids.create({'user_id': user_ids[0],
+                                            'oauth_access_token': params['access_token'],
+                                            'active_token': True,
+                                            })
         return res
+
+    @api.multi
+    @api.depends('oauth_access_max_token')
+    def clear_token(self):
+        for users in self:
+            for token in users.oauth_access_token_ids:
+                token.write({
+                                'oauth_access_token':"****************************",
+                                'active_token': False})
 
     @api.model
     def check_credentials(self, password):
@@ -54,8 +68,9 @@ class ResUsers(models.Model):
         except openerp.exceptions.AccessDenied:
             res = self.env['auth.oauth.multi.token'].sudo().search([
                 ('user_id', '=', self.env.uid),
-                ('oauth_access_token', '=', password)],
-                ('active', '=', True),
+                ('oauth_access_token', '=', password),
+                ('active_token', '=', True),]
+                ,
             )
             if not res:
                 raise
